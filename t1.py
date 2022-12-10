75f069f87b0dac5f2e59d165414fcc160519b02b
M4dhav <114633095+M4dhav@users.noreply.github.com>
2022-12-10 07:39:04 +0530

    nothing

--- t1.py
+++ t1.py
@@ -1,1 +1,8 @@
 from bs4 import BeautifulSoup
+import requests
+nyt = requests.get("https://www.nytimes.com")
+nyt_txt = nyt.text
+soup = BeautifulSoup(nyt_txt, 'html.parser')
+tag = soup.find_all('h3', {"class" : "indicate-hover css-1n70gp3"})
+for i in tag:
+    print(i.getText())
